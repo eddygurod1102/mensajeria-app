27bb87c006db59a6f73a6bb4da62a7b9c95d22cf
eddygurod1102 <guerecaedel@gmail.com>
2023-07-01 17:27:40 -0600

    Punto de montaje cambiado (etiqueta body)

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,9 @@
 <script setup>
   import Navbar from './Navbar.vue';
+  import Main from './Main.vue';
 </script>
 
 <template>
   <Navbar />
-  <main>
-    <h1>Est&aacute;s en la p&aacute;gina de inicio</h1>
-  </main>
+  <Main />
 </template>
--- src/main.js
+++ src/main.js
@@ -3,4 +3,4 @@ import './css/estilos.css';
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+createApp(App).mount('body');
