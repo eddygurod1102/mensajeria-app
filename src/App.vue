a76406a7f0568536163bd61bdedfe277987de3fc
eddygurod1102 <guerecaedel@gmail.com>
2023-07-01 14:28:07 -0600

    Commit inicial

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+  import Navbar from './Navbar.vue';
+</script>
+
+<template>
+  <Navbar />
+  <main>
+    <h1>Est&aacute;s en la p&aacute;gina de inicio</h1>
+  </main>
+</template>
---
+++ src/Navbar.vue
@@ -0,0 +1,11 @@
+<script setup>
+</script>
+
+<template>
+  <nav class="barra-navegacion barra-navegacion--dark">
+    <div class="barra-navegacion__enlaces">
+      <a class="barra-navegacion__enlaces__enlace" href="#">Enlace 1</a>
+      <a class="barra-navegacion__enlaces__enlace" href="#">Enlace 2</a>
+    </div>
+  </nav>
+</template>
